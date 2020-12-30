525fa99c4f3ff3db2c86cfaf2f85de2cc557d8a6
rftengineer <nightpw1@naver.com>
2020-12-30 20:18:57 +0900

    test: StringUtils.equals Method 작성

--- src/test/java/StringUtilsForApacheTest.java
+++ src/test/java/StringUtilsForApacheTest.java
@@ -27,4 +27,53 @@ public class StringUtilsForApacheTest {
     void apache_StringUtils_TestMethod3(String value) {
         assertThat(StringUtils.strip(value)).isEqualTo("S tr1");
     }
+
+    @Test
+    void apache_StringUtils_TestMethod4() {
+        String value = "";
+        assertThat(StringUtils.stripToNull(value)).isNull();
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"", "  ", " "})
+    @DisplayName("StripToNull의 용례 체크")
+    void apache_StringUtils_TestMethod5(String value) {
+        assertThat(StringUtils.stripToNull(value)).isNull();
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"xyzstryzx", "xystrz", "strzxyxzz"})
+    @DisplayName("stripChars 지정할 경우 작동하는 원리 파악")
+    void apache_StringUtils_TestMethod6(String value) {
+        assertThat(StringUtils.strip(value, "xyz")).isEqualTo("str");
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"xyzstr", "yzstr", "zxstr"})
+    void apache_StringUtils_Method7(String value) {
+        assertThat(StringUtils.stripStart(value, "xyz")).isEqualTo("str");
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"strxyz", "strzy", "strzx"})
+    void apache_StringUtils_Method8(String value) {
+        assertThat(StringUtils.stripEnd(value, "xyz")).isEqualTo("str");
+    }
+
+    @Test
+    void apache_StringUtils_Method9() {
+        String a1 = "sqrt";
+        String a2 = "sqrt";
+        assertThat(StringUtils.equals(a1, a2)).isTrue();
+    }
+
+    @Test
+    @DisplayName("null도 equals시 비교하는지 확인")
+    void apache_StringUtils_Method10() {
+        String a1 = null;
+        String a2 = null;
+        assertThat(StringUtils.equals(a1, a2)).isTrue();
+    }
+
+
 }
