62dcc5d76f290420067f5e067a34cd7f870b44e7
rftengineer <nightpw1@naver.com>
2020-12-31 23:53:16 +0900

    test: Jackson test code 구현, String To JsonNode, String to Map 연습

--- src/test/java/JacksonDatabindTest.java
+++ src/test/java/JacksonDatabindTest.java
@@ -1,3 +1,6 @@
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import dao.Car;
 import org.junit.jupiter.api.BeforeEach;
@@ -7,6 +10,7 @@ import org.junit.jupiter.api.Test;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -36,5 +40,25 @@ public class JacksonDatabindTest {
         String jsonData = "{ \"color\" : \"Black\", \"type\" : \"BMW\" }";
         Car car = objectMapper.readValue(jsonData, Car.class);
         assertThat(car.getColor()).isEqualTo("Black");
+    }
+
+    @Test
+    void jackson_jsonNode_test1() throws JsonProcessingException {
+        String jsonData = "{ \"color\" : \"Black\", \"type\" : \"BMW\" }";
+        JsonNode jsonNode = objectMapper.readTree(jsonData);
+
+        assertThat(jsonNode.get("color").asText()).isEqualTo("Black");
+
+    }
+
+    @Test
+    void jackson_javaMap_test() throws JsonProcessingException {
+        String jsonData = "{ \"color\" : \"Black\", \"type\" : \"BMW\" }";
+        Map<String, Object> map =
+                objectMapper.readValue(jsonData, new TypeReference<>() {
+                });
+
+        //System.out.println(map);
+        assertThat(map.get("color")).isEqualTo("Black");
     }
 }
